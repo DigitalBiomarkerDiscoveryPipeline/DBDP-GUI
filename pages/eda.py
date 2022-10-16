d6b6a6c33fbc46b2165f7c752c92a9ea117bb19e
Bill Chen <billchen@bills-mbp.lan>
2022-10-15 20:55:48 -0400

    error correlation plots added

--- pages/eda.py
+++ pages/eda.py
@@ -58,6 +58,25 @@ layout = html.Div(
                         dcc.Graph(id='heatmap')
                     ],
                     title="Explore Correlation"
+                ),
+                dbc.AccordionItem(
+                    [
+                        dbc.Row([
+                            dbc.Col([
+                                dcc.Dropdown(
+                                    ['Mean Directional Error',
+                                        'Mean Absolute Error'],
+                                    'Mean Directional Error',
+                                    id='error-type'
+                                )
+                            ]),
+                            dbc.Col([
+                                dcc.Dropdown(id='person-id-options')
+                            ]),
+                        ]),
+                        dcc.Graph(id='error_calc')
+                    ],
+                    title="Error Calculation"
                 )
             ]
         )
@@ -112,7 +131,12 @@ def draw_timeseries(df, time_col, standard, comp_y, person_to_plot):
     for i, y in enumerate(comp_y):
         add_fig = px.line(temp, x=time_col, y=y)
         add_fig.data[0].line.color = colors[i]
+        add_fig.data[0]['showlegend'] = True
+        add_fig.data[0]['name'] = y
         fig.add_trace(add_fig.data[0])
+
+    fig.data[0]['showlegend'] = True
+    fig.data[0]['name'] = standard
 
     return fig
 
@@ -173,5 +197,39 @@ def update_corr_table(user_uploaded_data):
 
     corr = temp.corr()
     fig = px.imshow(corr)
+
+    return fig
+
+# Error Calculation Plots
+
+
+def draw_error_plots(df, standard, wearables, error_type, person_id):
+    df = df[df['ID'] == person_id]
+    mde = df[wearables].sub(df[standard], axis=0)
+    mae = abs(mde.copy())
+    if error_type == 'Mean Directional Error':
+        fig = px.box(mde, y=wearables, labels={
+                     'value': error_type})
+    else:
+        fig = px.box(mae, y=wearables, labels={
+                     'value': error_type})
+
+    return fig
+
+
+@callback(
+    Output('error_calc', 'figure'),
+    Input('data-store', 'data'),
+    Input('error-type', 'value'),
+    Input('person-id-options', 'value')
+)
+def update_error_plots(user_uploaded_data, error_type, person_id):
+    df = pd.read_json(user_uploaded_data)
+
+    fig = draw_error_plots(df, 'ECG',
+                           ['Apple Watch', 'Empatica',
+                            'Garmin', 'Fitbit', 'Miband', 'Biovotion'],
+                           error_type,
+                           person_id)
 
     return fig
